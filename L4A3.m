b2530829115d2d743ad158d3d7d723a2d07b575f
tobia <tobia@Scratch-The-Suface.fritz.box>
2021-11-28 17:27:53 +0100

    mit Schleife

--- L4A3.m
+++ L4A3.m
@@ -10,6 +10,15 @@ C_3=5.8e-9;
 R_6=2200;
 
 simOut = sim('L4A3_simu.mdl',0.04);
+
+while max(get(simOut.ScopeData,1).Values.Data)<5
+    simOut = sim('L4A3_simu.mdl',0.1);
+    plot(get(simOut.ScopeData,1).Values.Time,get(simOut.ScopeData,1).Values.Data);
+%     C_2=C_2+1e-9;
+    C_3=C_3+1e-10;
+%     R_6=R_6+100;
+end
+
 max(get(simOut.ScopeData,1).Values.Data)
 hold on
 plot(get(simOut.ScopeData,1).Values.Time,get(simOut.ScopeData,1).Values.Data);
